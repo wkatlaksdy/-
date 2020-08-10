d976e9bfd00d7b0269a93352f68cb815205a9832
wkatlakdy <1_5_6_1@naver.com>
2020-08-10 18:36:39 +0900

    create react project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <img src={logo} lat="loge" />
+      <h2>Let's develop manmament system!</h2>
+
+    </div>
+  );
+}
+
+export default App;
